96e4e75918972076d335e9afd119bcdec31ef0e8
zuya <kazu.prgrm555y@gmail.com>
2025-12-09 20:50:08 +0900

    add: login function

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
 function Login() {
-  return <div>Login page</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = () => {};
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Your email"
+          value={email}
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Your password"
+          value={password}
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+      <button>Login</button>
+    </>
+  );
 }
 
 export default Login;
