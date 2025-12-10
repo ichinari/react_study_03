db49aeb163fa5275ba93c6de0e6faa5d9cf5ded3
zuya <kazu.prgrm555y@gmail.com>
2025-12-10 23:01:39 +0900

    add: 🎸 login function and style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,45 @@
-import { useState } from 'react';
+import { useState, type FormEvent } from 'react';
+import { supabase } from '@api/supabase';
+
 function Login() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleLogin = () => {};
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        console.error('Auth error:', error);
+      }
+
+      setEmail('');
+      setPassword('');
+
+      alert('login success');
+    } catch (error) {
+      console.error('Unexpected error:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={handleLogin} className="flex flex-col items-center gap-3">
         <input
           type="email"
           placeholder="Your email"
           value={email}
           required={true}
           onChange={(e) => setEmail(e.target.value)}
+          className="border border-gray-300 p-1.5 rounded-md"
         />
         <input
           type="password"
@@ -20,11 +47,16 @@ function Login() {
           value={password}
           required={true}
           onChange={(e) => setPassword(e.target.value)}
+          className="border border-gray-300 p-1.5 rounded-md"
         />
+        <button
+          disabled={loading}
+          className="border border-blue-200 bg-blue-200 p-1.5 rounded-md"
+        >
+          {loading ? <span>Loading</span> : <span>Login</span>}
+        </button>
       </form>
-      <button>Login</button>
     </>
   );
 }
-
 export default Login;
